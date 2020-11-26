d5f6265b19feda6b96c2788501a7521d2cde8f52
Arjun Sahni <arjun.sahni@nuance.com>
2020-11-26 17:39:28 -0500

    put auth token in auth.txt for security

--- medical_api.py
+++ medical_api.py
@@ -11,7 +11,8 @@ class ApiClass:
     class for api
     """
     base_url = "https://sandbox-healthservice.priaid.ch"
-    sandbox_auth_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImFyanVuc2Fobmljb2xsZWdlQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNzk0NSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAyMC0xMC0xMyIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjA2NDI1MjA5LCJuYmYiOjE2MDY0MTgwMDl9.l44Edt7evULKv0xA8Dgs8gCdTlhx7QuBSmPKTM8cpmU"
+    def __init__(self):
+        self.sandbox_auth_token = open('auth.txt', 'r').readline()
 
     def get_symptoms(self):
         symptom_url = f'{self.base_url}/symptoms?token={self.sandbox_auth_token}&language=en-gb'
