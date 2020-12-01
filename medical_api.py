75ef77fcd702d4b0a1cd3cc157d231b320466a73
Arjun Sahni <arjun.sahni@nuance.com>
2020-12-01 15:08:33 -0500

    updated functions

--- medical_api.py
+++ medical_api.py
@@ -4,6 +4,7 @@ Class for api
 import requests
 import ast
 import unicodedata
+import json
 
 
 class ApiClass:
@@ -17,7 +18,7 @@ class ApiClass:
     def get_symptoms(self):
         symptom_url = f'{self.base_url}/symptoms?token={self.sandbox_auth_token}&language=en-gb'
         response = requests.get(symptom_url)
-        print(response.content)
+        return response.content
 
     def get_symptom_id(self, name):
         data_string = open('all_symptoms.json', 'r').read().replace('\n', ' ')
@@ -32,13 +33,24 @@ class ApiClass:
         diagnosis_url = f'{self.base_url}/diagnosis?symptoms={str(symptom_id_list)}&gender={gender.lower()}&year_of_birth={str(yob)}&token={self.sandbox_auth_token}&language=en-gb'
         response = requests.get(diagnosis_url)
         response_str = response.content.decode("UTF-8")
-        return ast.literal_eval(response_str)
+        normalized_str = unicodedata.normalize('NFKD', response_str).encode('ascii', 'ignore').decode("UTF-8")
+        return ast.literal_eval(normalized_str)
 
     def get_issue_info(self, issue_id):
         issue_url = f'{self.base_url}/issues/{str(issue_id)}/info?token={self.sandbox_auth_token}&language=en-gb'
         #issue_url = f'{self.base_url}/issues/324/info?token={self.sandbox_auth_token}&language=en-gb'
         response = requests.get(issue_url)
         response_str = response.content.decode("UTF-8")
-        return unicodedata.normalize('NFKD', response_str).encode('ascii', 'ignore')
+        normalized_str = unicodedata.normalize('NFKD', response_str).encode('ascii', 'ignore').decode("ascii")
+        return json.loads(normalized_str)
+
+
+    def get_more_symptoms(self, symptom_id_list, gender, yob):
+        url = f'{self.base_url}/symptoms/proposed?token={self.sandbox_auth_token}&language=en-gb&symptoms={str(symptom_id_list)}&gender={gender.lower()}&year_of_birth={str(yob)}'
+        response = requests.get(url)
+        response_str = response.content.decode("UTF-8")
+        normalized_str = unicodedata.normalize('NFKD', response_str).encode('ascii', 'ignore').decode("UTF-8")
+        return ast.literal_eval(normalized_str)
+
 
 
